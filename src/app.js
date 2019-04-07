382b48ad50c9b17ec84c4449ed90e7fdf626e607
harsh <hkundnani@gmail.com>
2019-04-07 12:15:48 -0400

    Add a class component by replacing function component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
-const App = () => {
-    return React.createElement("div", {},
-        React.createElement("h1", {}, 'Adopt a pet!')
-    )
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
+    ]);
 }
+
+class App extends React.Component {
+    handleTitleClick() {
+        alert("you clicked this title");
+    }
+    render() {
+        return React.createElement("div", {}, [
+            React.createElement("h1", {onClick: this.handleTitleClick}, 'Adopt a pet!'),
+            React.createElement(Pet, {
+                name: "Dog1",
+                animal: "Dog",
+                breed: "German Spheard"
+            }),
+            React.createElement(Pet, {
+                name: "Bird1",
+                animal: "Bird",
+                breed: "Sparrow"
+            }),
+            React.createElement(Pet, {
+                name: "Cat1",
+                animal: "Cat",
+                breed: "Mixed"
+            })
+        ])
+    }
+}
+
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
